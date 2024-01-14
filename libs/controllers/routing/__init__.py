9e67cf23049a37888fb06fd107ee0a942285c1c0
Pjotr Kooijmans <kooijmans97@gmail.com>
2024-01-14 10:44:08 +0100

    Add routing functionality and handle route requests and replies

--- libs/controllers/routing/__init__.py
+++ libs/controllers/routing/__init__.py
@@ -1,22 +1,118 @@
+from libs.controllers.config.NodeConfigData import NodeConfigData
 from libs.controllers.neighbours import NeighboursController
-from libs.controllers.network import INetworkController
+from libs.controllers.network import Frame, INetworkController
+from libs.controllers.network import Frame
+
 
 
 class RoutingController:
-    def __init__(self, neighboursController: NeighboursController, networkController: INetworkController) -> None:
+    def __init__(self, node_config: NodeConfigData, neighboursController: NeighboursController, networkController: INetworkController) -> None:
+        self.node_config = node_config
         self.neighbours = neighboursController
         self.network = networkController
+        self.routes: dict[int, tuple[int, int]] = {}
 
     async def getRoute(self, address: int):
-        """
+            """
             Get the route to a node, returns the address of the next connected node.
 
-            When a node is out of reach of the network, this function will return -1.
+            Args:
+                address (int): The address of the node to find the route to.
+
+            Returns:
+                int: The address of the next connected node in the route.
+
+            Notes:
+                If the node is out of reach of the network, this function will send out a route_request to all neighbours and return -1.
+            """
+
+            if address in self.routes:
+                print("address found in routes")
+                return self.routes[address][0]
+            else:
+                print("address not found in routing table")
+                frame = Frame(Frame.FRAME_TYPES['route_request'], self.node_config.serialize(), self.node_config.addr, address)
+                self._forward_to_all_neighbours(frame)
+                return -1
+        
+    def send_route_request(self, frame: Frame, address: int):
+        self.network.send_message(
+            Frame.FRAME_TYPES['route_request'], self.node_config.serialize(), address
+        )
+
+    def send_route_reply(self, frame: Frame, address: int):
+        self.network.send_message(
+            Frame.FRAME_TYPES['route_response'], self.node_config.serialize(), address
+        )
+
+
+    def handle_routing_request(self, frame: Frame):
         """
-        return address
+        Handles a routing request frame.
 
-    def handle_routing_request(self):
-        pass
+        Args:
+            frame (Frame): The routing request frame to handle.
 
-    def handle_routing_response(self):
-        pass
+        Returns:
+            None
+        """
+        if frame.type != Frame.FRAME_TYPES['route_request']:
+            return
+        
+        address = self.node_config.addr
+        source = Frame.deserialize(frame.source_address)
+        destination = Frame.deserialize(frame.destination_address)
+        last_address = NodeConfigData.deserialize(frame.data).addr
+
+        if source not in self.routes:
+            self.routes[source] = last_address
+
+        if address == destination:
+            print("sending routing response")
+            frame = Frame(Frame.FRAME_TYPES['route_response'], self.node_config.serialize, destination, source)
+            self.send_route_reply()
+        elif destination in self.neighbours.connections:
+            print("destination is a neighbour")
+            self.send_route_request(frame, destination) 
+        elif destination in self.routes:
+            self.send_route_request(frame, self.getRoute(destination))
+        else:
+            self._forward_to_all_neighbours(frame)
+            
+
+    def handle_route_reply(self, frame: Frame):
+        """
+        Handles the route reply frame received by the node.
+
+        Args:
+            frame (Frame): The route reply frame to be handled.
+
+        Returns:
+            None
+        """
+        if frame.type != Frame.FRAME_TYPES['route_reply']:
+            return
+
+        address = self.node_config.addr
+        destination = Frame.deserialize(frame.destination_address)
+        source = Frame.deserialize(frame.source_address)
+        last_address = NodeConfigData.deserialize(frame.data).addr
+
+        if source not in self.routes:
+            self.routes[source] = last_address
+
+        if address != destination:
+            self.send_route_reply(frame, self.getRoute(destination))
+        
+    def _forward_to_all_neighbours(self, frame: Frame):
+        """
+        Forwards the given frame to all neighbours in the network.
+
+        Args:
+            frame (Frame): The frame to be forwarded.
+
+        Returns:
+            None
+        """
+        for neighbour in self.neighbours.connections:
+            self.network.send_message(frame.type, frame.data, neighbour)
