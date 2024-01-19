2b8e0a450dcebcc27cb24976fcf70aad084971f2
Jonathan <32264711+JonaVDM@users.noreply.github.com>
2024-01-19 16:05:25 +0100

    i fixed it

--- libs/controllers/timekeeping/__init__.py
+++
@@ -1,7 +0,0 @@
-
-class ITimekeepingController:
-    def get_time(self):
-        raise NotImplementedError()
-
-    def sync_time(self, unix_timestamp: int):
-        raise NotImplementedError()
--- libs/sensors/__init__.py
+++
@@ -1,3 +0,0 @@
-class ISensor:
-    def get_measurement(self) -> dict[str, int]:
-        raise NotImplementedError('class {} does not implement get_measurement()'.format(self.__class__.__name__))
