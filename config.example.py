4751066e9296a382c44f1c213c489e479f243c78
sherlock123401234 <97603861+sherlock123401234@users.noreply.github.com>
2024-01-21 11:31:26 +0100

    Renamed config.py to config.example.py

---
+++ config.example.py
@@ -0,0 +1,12 @@
+address = b'\x00\x03'
+baud_rate = 9600  # 1200, 2400, 4800, 9600, 19200, 38400, 57600, 115200
+air_rate = 2.4  # 2.4 4.8, 9.6, 19.2, 38.4, 62.5
+transmit_power = 10  # 22, 17, 13, 10
+channel = 4  # 0-80
+rssi_enabled = True  # True, False
+transmission_method = True  # True = Transparent, False = Fixed
+lbt_enabled = True  # True, False
+serial_parity_bit = '8N1'  # 8N1, 8O1, 8E1
+sub_packet = 200  # 200, 128, 64, 32
+rssi_ambient_noice_enable = False  # False, True
+wor_cycle = 2000  # 500, 1000, 1500, 2000, 2500, 3000, 3500, 4000,
