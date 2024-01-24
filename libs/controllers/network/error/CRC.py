325a11fb6ff662b6f3271bba9626c769bde91998
_Slubby_ <pmhuberts@gmail.com>
2024-01-24 11:18:18 +0100

    Use other crc version

--- libs/controllers/network/error/CRC.py
+++ libs/controllers/network/error/CRC.py
@@ -2,6 +2,7 @@ from libs.external.reedsolo import *
 
 import array
 
+
 class CRC:
     def __init__(self):
         self.corrector = RSCodec(5)
@@ -12,14 +13,14 @@ class CRC:
         crc = c
         for j in range(8):
             if crc & 0x01:
-                crc = (crc >> 1) ^ 0xA001
+                crc = (crc >> 1) ^ 0x8408
             else:
                 crc = crc >> 1
 
         return crc
 
     def checksum(self, data: bytes) -> bytes:
-        crc = 0xFFFF
+        crc = 0x0000
         for c in data:
             crc = (crc >> 8) ^ self.table[(crc ^ c) & 0xFF]
 
@@ -49,25 +50,25 @@ class CRC:
             return None
 
 # Testing
-# crc = CRC()
-#
-# # Encode data
-# original_data = b"hello world"
-# encoded_data = crc.encode(original_data)
-# print("Encoded data:", encoded_data)
-#
-# print(len(encoded_data))
-#
-# # Simulate errors
-# encoded_data[2] ^= 0xFF  # introduce an error
-# encoded_data[5] ^= 0xFF  # introduce an error
-#
-# print("corrupt data:", encoded_data)
-#
-# # Decode data
-# decoded_data = crc.decode(encoded_data)
-#
-# if decoded_data is not None:
-#     print("Decoded data:", decoded_data)
-# else:
-#     print("Error: Unable to correct the data.")
+crc = CRC()
+
+# Encode data
+original_data = b"hello world"
+encoded_data = crc.encode(original_data)
+print("Encoded data:", encoded_data)
+
+print(len(encoded_data))
+
+# Simulate errors
+encoded_data[2] ^= 0xFF  # introduce an error
+encoded_data[5] ^= 0xFF  # introduce an error
+
+print("corrupt data:", encoded_data)
+
+# Decode data
+decoded_data = crc.decode(encoded_data)
+
+if decoded_data is not None:
+    print("Decoded data:", decoded_data)
+else:
+    print("Error: Unable to correct the data.")
